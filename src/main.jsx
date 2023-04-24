a4d4caddf45497b5f4b55713fa356dc67d6061d3
RichardOrlandini <richardorlandini09@gmail.com>
2023-04-24 14:14:28 -0300

    adding auth imports and routes in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {ThemeProvider} from 'styled-components'
-import theme from './styles/theme'
-import GlobalStyle from './styles/global'
+import {ThemeProvider} from 'styled-components';
+import { AuthProvider } from './hooks/auth';
+import GlobalStyle from './styles/global';
+import ReactDOM from 'react-dom/client';
+import theme from './styles/theme';
+import { Routes } from './routes';
+import React from 'react';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle/>
+      <AuthProvider>
         <Routes/>
+        </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
